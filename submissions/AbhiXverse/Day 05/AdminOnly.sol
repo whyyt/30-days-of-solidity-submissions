4d1cf9640cd847a69e1512e568a1d6db28ef9092
Abhishek Soni <abhishek@Abhisheks-MacBook-Air.local>
2025-04-05 23:08:01 +0530

    Day05 - Submission by @AbhiXverse

--- submissions/AbhiXverse/Day 05/AdminOnly.sol
+++ submissions/AbhiXverse/Day 05/AdminOnly.sol
@@ -3,31 +3,34 @@
 pragma solidity ^0.8.18;
 
 contract AdminOnly {
+ 
+    address public owner;                                    // address of the owner 
+    uint256 public totaltreasure;                            // total treasure amount 
 
-    address public owner;
-    uint256 public totaltreasure;
+    mapping(address => uint256) public allowed;              // how much treasure each user allowed to take        
+    mapping(address => bool) public alreadytaken;            // tracks if the user has already taken the treasure 
 
-    mapping(address => uint256) public allowed;
-    mapping(address => bool) public alreadytaken;
-
-    constructor() {
-        owner = msg.sender;
-    }
+    constructor() { 
+        owner = msg.sender;                                  // set the deployer as the owner 
+    } 
 
     modifier admin() {
-        require(owner == msg.sender, "only owner");
+        require(owner == msg.sender, "only owner");          // only owner can call the function 
         _;
     }
 
+    // function to add the trasure to the contract 
     function addtreasure() public payable admin {
         totaltreasure += msg.value;
     }
 
+    // owner allows a specific user to get treasure 
     function allowUser(address user, uint256 amount) public admin {
         require( amount <= address(this).balance, "not enough treasure");
         allowed[user] = amount;
     }
 
+    // function to get the trasure if allowed and havn't taken already 
     function gettreasure() public {
         require(allowed[msg.sender] > 0, "Not allowed to get treasure");
         require(!alreadytaken[msg.sender], "already get the treasure");
@@ -37,14 +40,17 @@ contract AdminOnly {
         payable(msg.sender).transfer(amount);
     }
 
+    // function to check the current treasure balance 
     function checktreasureBalance() public view returns(uint256) {
         return address(this).balance;
     }
 
+    // owner can reset a user's withdrawal status (let them withdraw again)
     function resertetTreasureStatus(address user) public admin {
         alreadytaken[user] = false;
     }
-
+    
+    // owner can transfer control of the treasure contract or chest to someone else
     function transferOwnership(address newOwner) public admin {
         require (newOwner != address(0), "Invalid address");
         owner = newOwner;
