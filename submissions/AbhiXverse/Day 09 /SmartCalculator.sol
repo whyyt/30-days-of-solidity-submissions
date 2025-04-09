e5c781fc90fbeba7aa7791f0f8337ccc9d92e139
Abhishek Soni <abhishek@Abhisheks-MacBook-Air.local>
2025-04-09 18:52:14 +0530

    Day09 - Submission by @AbhiXverse

--- submissions/AbhiXverse/Day 09 /SmartCalculator.sol
+++ submissions/AbhiXverse/Day 09 /SmartCalculator.sol
@@ -30,30 +30,35 @@ contract SimpleCalculator {
   
 contract SmartCalculator {
 
-    SimpleCalculator public SimpleCalc;
+    address public simpleCalculatorAddress;
 
     // constructor to set the SimpleCalculator contract address
-    constructor(address smartCalculatorAddress) {
-        SimpleCalc = SimpleCalculator(smartCalculatorAddress);
+    constructor(address _simpleCalculatorAddress) {
+        simpleCalculatorAddress = _simpleCalculatorAddress;
     } 
     
     // function to get the sum of two numbers
     function getAdd(uint256 x, uint256 y) public view returns (uint256) {
-        return SimpleCalc.Add(x, y);
+        SimpleCalculator calculator = SimpleCalculator(simpleCalculatorAddress);
+        return calculator.Add(x, y);
     }
 
     // function to get the difference of two numbers
     function getDifference(uint256 x, uint256 y) public view returns (uint256) {
-        return SimpleCalc.Sub(x, y);
+        SimpleCalculator calculator = SimpleCalculator(simpleCalculatorAddress);
+        return calculator.Sub(x, y);
     }
 
     // function to get the product of two numbers
     function getproduct(uint256 x, uint256 y) public view returns (uint256) {
-        return SimpleCalc.Multi(x, y);
+        SimpleCalculator calculator = SimpleCalculator(simpleCalculatorAddress);
+        return calculator.Multi(x, y);
     }
 
     // function to get the  of two numbers
     function getQuotient(uint256 x, uint256 y) public view returns (uint256) {
-        return SimpleCalc.Div(x, y);
+        SimpleCalculator calculator = SimpleCalculator(simpleCalculatorAddress);
+        return calculator.Div(x, y);
+
     }
 }
