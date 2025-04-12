4032e34bcd4f570c9e6fdb926ba2971aa33a263b
Yash Verma <recognizeyourself909@gmail.com>
2025-04-12 20:20:57 +0530

    feat: Add MyFirstToken contract with basic ERC20 functionality
    This commit introduces the MyFirstToken contract, a basic implementation of an ERC20 token.
    
    The changes include:
    
    - Implementation of core ERC20 functionalities such as `transfer`, `approve`, and `transferFrom`.
    - Definition of token metadata including `name`, `symbol`, and `decimal`.
    - Implementation of `balanceOf` and `allowance` mappings to track token balances and allowances.
    - Implementation of `Transfer` and `Approval` events.
    - A constructor to set the initial token supply and assign it to the contract owner.

---
+++ submissions/Yash-verma18/MyFirstToken.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyFirstToken {
+    address public owner;
+    uint256 public totalSupply = 100 * 10 ** 18;
+
+    string public name ;
+    string public symbol ;
+    uint8 public constant decimal = 18;
+
+    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor (string memory _tokenName, string memory _symbol) {    
+        owner = msg.sender;
+        balanceOf[msg.sender] = totalSupply;
+        name = _tokenName;
+        symbol = _symbol;
+    }
+
+    function transfer (address _to, uint256 _amount) public {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _amount, "Not enough tokens");
+       
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+    }
+
+    function approve (address _spender, uint256 _amount) public returns (bool) {
+        allowance[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transferFrom (address _from, address _to ,uint256 _amount) public {
+        require(_to != address(0), "Invalid reciepient address");
+        require(allowance[_from][msg.sender] >= _amount, "Not enough allowance tokens");
+        require(balanceOf[_from] >= _amount, "Not enough tokens");
+
+        balanceOf[_from] -= _amount;
+        balanceOf[_to] += _amount;
+        allowance[_from][msg.sender] -= _amount;
+        emit Transfer(_from, _to, _amount);
+    }
+
+}
