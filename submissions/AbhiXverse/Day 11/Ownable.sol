572aaa740d4cfea6f6d8918a75e64136aa7524da
Abhishek Soni <abhishek@Abhisheks-MacBook-Air.local>
2025-04-14 21:01:16 +0530

    Day11 - Submission by @AbhiXverse

---
+++ submissions/AbhiXverse/Day 11/Ownable.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.18;
+
+contract Ownable {
+
+    address public owner;
+
+    event OwnershipTransfer(address indexed previousOwner, address newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier Admin() {
+        require(owner == msg.sender, "Only Admin can call");
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public Admin {
+        require(_newOwner != address(0), "Invalid Address");
+        address previous = owner;
+        owner = _newOwner;
+        emit OwnershipTransfer(previous, _newOwner);
+    }
+}
