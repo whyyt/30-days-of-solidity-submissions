616b82285c9395788233e134f242aad5f78eda06
Abhishek Soni <abhishek@Abhisheks-MacBook-Air.local>
2025-04-23 21:01:51 +0530

    Day17 - Submission by @AbhiXverse

---
+++ submissions/AbhiXverse/Day 17 /SubscriptionLogicV1.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.18;
+
+import "./SubscriptionStorageLayout.sol";
+
+contract SubsscriptionLogicV1 is SubscriptionStorageLayout {
+
+    function addplan(uint8 planId, uint256 price, uint256 duration) external {
+        planPrices[planId] = price;
+        planDuration[planId] = duration;
+    }
+
+    function subscribe(uint8 planId) external payable {
+        require(planPrices[planId] > 0," Invalid plan ");
+        require(msg.value >= planPrices[planId], "Insufficient funds");
+
+        Subscription storage s = subscriptions[msg.sender];
+        if(block.timestamp < s.expiry) {
+            s.expiry += planDuration[planId];
+        }
+        else {
+            s.expiry = block.timestamp + planDuration[planId];
+        }
+
+        s.planId = planId;
+        s.paused = false;
+    }
+
+    function isActive(address user) external view returns (bool) {
+        Subscription memory s = subscriptions[user];
+        return (block.timestamp < s.expiry && !s.paused);
+    }
+
+}
