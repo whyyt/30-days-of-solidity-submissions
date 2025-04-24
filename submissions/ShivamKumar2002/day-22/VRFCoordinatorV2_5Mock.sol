c15cd38dbfd09307fb1a05107fede3ced370c962
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2025-04-25 00:55:18 +0530

    shivam: Submission for Day 22, Decentralized Lottery Smart Contract
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

---
+++ submissions/ShivamKumar2002/day-22/VRFCoordinatorV2_5Mock.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+// NOTE: See https://docs.chain.link/vrf/v2-5/subscription/test-locally for more information about the mock
