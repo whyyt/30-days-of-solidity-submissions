3f05b9e4570fd6de297b63b24c5ed4d0bf696444
segment7 <eszxcde@hotmail.com>
2025-06-08 21:12:49 +0800

    D8+fix D5

--- submissions/segment7/5-AdminOnly.sol
+++ submissions/segment7/5-AdminOnly.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract AdminOnly {
     // State variables
     address public owner;
-    uint256 public treasureAmount;
+    uint256 private treasureAmount;
     mapping(address => uint256) public withdrawalAllowance;
     mapping(address => bool) public hasWithdrawn;
     
