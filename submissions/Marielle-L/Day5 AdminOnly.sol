5cf114e3185ef2a5c21d1b19b4caabc895a6da39
Hysteriaa <moonshowmaker@gmail.com>
2025-06-17 17:19:06 +0800

    Update Day5 AdminOnly.sol

--- submissions/Marielle-L/Day5 AdminOnly.sol
+++ submissions/Marielle-L/Day5 AdminOnly.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 contract AdminOnly {
     address public owner;
     uint256 public treasureAmount;
+    uint public cooldownTime;
     mapping(address => uint256) public withdrawalAllowance;
     mapping(address => bool) public hasWithdrawn;
+    mapping(address => uint) public lastWithdrawTime;
 
     constructor() {
         owner=msg.sender;
@@ -16,6 +18,12 @@ contract AdminOnly {
         _;
     }
 
+//Set Cooldown Time
+    function addCooldownTime(uint _cooldownTime) public onlyOwner{
+        cooldownTime = _cooldownTime;
+    }
+
+
     function addTreasure(uint256 amount) public onlyOwner {
         treasureAmount += amount;
     }
@@ -25,31 +33,38 @@ contract AdminOnly {
         withdrawalAllowance[recipient] = amount;
     }
 
-function withdrawTreasure(uint256 amount) public {
+    function withdrawTreasure(uint256 amount) public {
 
+//The Owner Withdraw
         if(msg.sender == owner){
             require(amount <= treasureAmount, "Not enough treasury available for this action.");
             treasureAmount-= amount;
-
             return;
         }
+
+// A Regular User Withdraw
         uint256 allowance = withdrawalAllowance[msg.sender];
-        
         require(allowance > 0, "You don't have any treasure allowance");
         require(!hasWithdrawn[msg.sender], "You have already withdrawn your treasure");
+        require(block.timestamp >= lastWithdrawTime[msg.sender] + cooldownTime, "Cannot withdraw before cooldownTime");
+        require(block.timestamp >= lastWithdrawTime[msg.sender] + cooldownTime, "Cannot withdraw before cooldownTime");
         require(allowance <= treasureAmount, "Not enough treasure in the chest");
         require(allowance >= amount, "Cannot withdraw more than you are allowed"); // condition to check if user is withdrawing more than allowed
-        
+
+// Finish the Withdrawl
         hasWithdrawn[msg.sender] = true;
         treasureAmount -= allowance;
         withdrawalAllowance[msg.sender] = 0;
         
     }
 
-   function resetWithdrawalStatus(address user) public onlyOwner {
+
+//Resetting a User’s Withdrawal Status
+    function resetWithdrawalStatus(address user) public onlyOwner {
         hasWithdrawn[user] = false;
     }
 
+//转移权限（所有权）
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "Invalid address");
         owner = newOwner;
