37280ea3d14add1b8d95b023a3d263893f34f7a2
Ruu0w0 <ruu07059@gmail.com>
2025-06-18 03:13:15 +0800

    Day17-SubscriptionLogicV2.sol

---
+++ submissions/Ruu/Day17-SubscriptionLogicV2.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import "./SubscriptionStorageLayout.sol";
+
+contract SubscriptionLogicV2 is SubscriptionStorageLayout{
+
+    function AddPlan(uint8 PlanId, uint256 price, uint256 duration) external{
+        PlanPrices[PlanId] = price;
+        PlanDuration[PlanId] = duration;
+    }
+
+    function Subscribe(uint8 PlanId) external payable{
+        require(PlanPrices[PlanId] > 0, "Invalid plan");
+        require(msg.value >= PlanPrices[PlanId], "Insufficient funds");
+
+        Subscription storage S = subscriptions[msg.sender];
+        if(block.timestamp < S.Expiry){
+            S.Expiry += PlanDuration[PlanId];
+        }
+        else{
+            S.Expiry = block.timestamp + PlanDuration[PlanId];
+        }
+
+        S.PlanId = PlanId;
+        S.paused = false;
+    }
+
+    function isActive(address user) external view returns(bool){
+        Subscription memory S = subscriptions[user];
+        return(block.timestamp < S.Expiry && !S.paused);
+    }
+
+    function PauseAccount(address user) external {
+        subscriptions[user].paused = true;
+    }
+
+    function ResumeAccount(address user) external {
+        subscriptions[user].paused = false;
+    }
+    
+}
