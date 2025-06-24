efa6c3098ed56fd8348c1bdf013aca5081e3835a
lyhyoooo <107075177+Yu-heng-Li@users.noreply.github.com>
2025-06-24 05:17:01 +0000

    Day 1

--- submissions/liyuheng/Day1 clickcounter.sol
+++ submissions/liyuheng/Day1 clickcounter.sol
@@ -1,12 +1,20 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
-contract ClickCounter{
 
+///@title 点击计数器合约
+///@author yuheng
+///@notice 此合约用以记录点击按钮的次数
+///@dev 用于solidity教学
+
+contract ClickCounter {
+
+    ///@notice 声明了一个只能存放正整数的全局的公开计数器的变量
     uint256 public counter;
 
-    function click() public{
-        counter++;
-
+    ///@notice 函数 点击 公开调用，每调用一次，该函数会将点击计数器 +1
+    /// @dev 没有限制调用者，也没有 Gas 限制，适合教学或测试
+    function click() public {
+        counter ++;
     }
+
 }
